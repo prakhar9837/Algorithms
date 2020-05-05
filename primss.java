4f4124770d27effb6fd6d4ad9c9dcf7e9eb8b3cd
prakhar9837 <34441769+prakhar9837@users.noreply.github.com>
2020-05-06 01:40:01 +0530

    Update primss.java
    added comments

--- primss.java
+++ primss.java
@@ -6,14 +6,14 @@ class primss {
     static int a, b, u, v;
 
     public static void main(String[] args) {
-        int visited[] = new int[10];
-        int cost[][] = new int[10][10];
-        int min, mincost = 0;
+        int visited[] = new int[10]; //this will check whether that vertex is visited or not. 
+        int cost[][] = new int[10][10]; //cost adjacency matrix
+        int min, mincost = 0; 
         System.out.println("\n Enter the number of nodes:");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         for (i = 1; i <= n; i++)
-            visited[i] = 0;
+            visited[i] = 0; //initially no vertex is visited.
         System.out.println("\n Enter the adjacency matrix:\n");
         for (i = 1; i <= n; i++)
             for (j = 1; j <= n; j++) {
@@ -21,7 +21,7 @@ class primss {
                 if (cost[i][j] == 0)
                     cost[i][j] = 999;
             }
-        visited[1] = 1;
+        visited[1] = 1; //starting node is considered as visited
         while (ne < n) {
             min = 999;
             for (i = 1; i <= n; i++) {
@@ -29,20 +29,20 @@ class primss {
                     if (cost[i][j] < min) {
                         if (visited[i] != 0) {
                             min = cost[i][j];
-                            a = u = i;
-                            b = v = j;
+                            a = u = i; //this will contain the row of the minimum vertex
+                            b = v = j; //this will contain the column of the minimum vertex
                         }
                     }
                 }
             }
-            if (visited[u] == 0 || visited[v] == 0) {
-                System.out.println(" Edge" + ne + ":" + u + "," + v + "cost:" + min);
+            if (visited[u] == 0 || visited[v] == 0) { 
+                System.out.println(" Edge" + ne + ":" + u + "," + v + "cost:" + min); //this will print the edges
                 ne++;
                 mincost += min;
                 visited[b] = 1;
             }
-            cost[a][b] = cost[b][a] = 999;
+            cost[a][b] = cost[b][a] = 999; //visited edges needs to be changes to infinity
         }
-        System.out.println(" Minimun cost=" + mincost);
+        System.out.println(" Minimun cost=" + mincost); //prints the minimum cost.
     }
 }
